ec8334cf31625fbcd2ca0207c87dae4b0ea6d198
Wiam-SAIBI <wiam.saibi2004@gmail.com>
2023-04-18 06:47:19 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,14 @@
 #include "dog.h"
+/**
+  * init_dog - Initializes a dog structure
+  * @d: A dog structure.
+  * @name: The name of the dog.
+  * @age: The age of the dog.
+  * @owner: The owner of the dog.
+  *
+  * Return: void
+  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
